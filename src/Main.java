dbb870bcdf1a87ffbd38b281c37fa85eaf5f7081
Completed_Part_1 <beechaah@mail.uc.edu>
2024-09-19 11:16:03 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int num = 2;
+
+        System.out.println("Enter a Number: ");
+
+        if (num % 2 == 0) {
+            System.out.println(num + " is even");
+        } else {
+            System.out.println(num + " is odd");
+        }
+    }
+}
